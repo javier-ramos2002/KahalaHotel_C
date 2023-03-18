0f93ae3d1bed7c394b3e234d6ba7bb49c10b98db
javier-ramos2002 <javier-ramos@opendeusto.es>
2023-03-18 12:14:13 +0100

    Estética

--- KahalaHotel.c
+++ KahalaHotel.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 int main() {
-   printf("*******************************\nBIENVENIDO AL GRAN KAHALA HOTEL\n*******************************\n");
    menuPrincipal();
    return 0;
 }
