8056dcaf573e4962ffb80847e7bdde7d4a6fc3c4
alejandropelegrin <79971607+alejandropelegrin@users.noreply.github.com>
2023-04-05 13:15:21 +0200

    db

--- bd.c
+++ bd.c
@@ -21,6 +21,31 @@ int init_database() {
 	sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
 	sqlite3_step(stmt);
 	sqlite3_finalize(stmt);
+
+    sprintf(sql, "INSERT INTO Cliente(dni, nombre, apellido, edad, telefono, numeroTarjeta, contrasena) VALUES ('%s', '%s', '%s', %d, %d, %d, '%s')", dni, nom, ape, edad, telefono, numeroTarjeta, con);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    result = sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
+
+   	sprintf(sql, "SELECT * FROM Cliente");
+    sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        printf("%s|", sqlite3_column_text(stmt, 0));
+        printf("%s|", sqlite3_column_text(stmt, 1));
+        printf("%s|", sqlite3_column_text(stmt, 2));
+        printf("%d|", sqlite3_column_int(stmt, 3));
+        printf("%d|", sqlite3_column_int(stmt, 4));
+        printf("%d|", sqlite3_column_int(stmt, 5));
+        printf("%s\n", sqlite3_column_text(stmt, 6));
+    }
+
+    sqlite3_finalize(stmt);
+
+	sprintf(sql, "delete from Cliente where dni = ?");
+    sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+
+    return SQLITE_OK;
 	
 }
 
